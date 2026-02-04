b3a89d3dfad2ebd0ded0b3a72f90923bc866c225
Junaidmdv <juni313786@gmail.com>
2026-02-05 00:06:20 +0530

    created sample fetch api

--- fetchapi/async.js
+++ fetchapi/async.js
@@ -1,21 +1,22 @@
 
 
 
-async function FetchData(){
-   try{
-       let responce= await fetch("https://fakestoreapi.com/products",{
-        method:"GET"
-       })    
-       let data= await responce.json() 
+async function GetProduct(){
+       try{ 
+          let response= await fetch("https://fakestoreapi.com/products")   
+          
+           if (!response.ok){
+               throw new Error("products not found")
+           }  
 
-       if (!responce.ok){
-            throw  new Error("data is not found")
-       } 
+           let data= await response.json() 
 
-       console.log(data)
-   }catch(error){
-    
-       console.log(error.message)
+           console.log(data)
 
-   }
-}
+       }catch(error){
+         
+             console.log(error.message)
+       }
+}  
+
+GetProduct(); 
